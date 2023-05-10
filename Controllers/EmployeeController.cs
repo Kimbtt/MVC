e6be3b467e6badce4b70ab91aa4e635f6cc1c760
Kimbtt <buitrongkim214@gmail.com>
2023-05-10 18:05:06 +0900

    Initial commit

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using FirstMvcProject.Models;
+
+namespace FirstMvcProject.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            List<Employee> employeeList = new List<Employee>();
+            Employee employee = new Employee();
+            employee.ID = 1;
+            employee.Name = "KIm";
+            employee.Salary = 1000;
+            employeeList.Add(employee);
+            
+            Employee employee2 = new Employee();
+            employee2.ID = 2;
+            employee2.Name = "Bui";
+            employee2.Salary = 6000;
+            employeeList.Add(employee2);
+
+            return View(employeeList);
+        }
+
+        public ActionResult AddEmployee()
+        {
+            Employee emp = new Employee();
+            return View(emp);
+        }
+
+        [HttpPost]
+        public ActionResult AddEmployee(Employee employee)
+        {
+            return View();
+        }
+
+        public ActionResult UpdateEmployee()
+        {
+            Employee employee = new Employee();
+            employee.ID = 1;
+            employee.Name = "dfsfdsd";
+            employee.Salary = 1000;
+            return View(employee);
+        }
+        [HttpPost]
+        public ActionResult UpdateEmployee(Employee employee)
+        {
+            
+            return View(employee);
+        }
+    }
+}
